ae63525e2725c64362a1af5aa31107010415b3cf
branden kennedy <brandenk88@gmail.com>
2025-03-25 08:48:20 -0700

    changed tag to "barrel" instead of "Barrel"

--- Assets/Scripts/GroundKill.cs
+++ Assets/Scripts/GroundKill.cs
@@ -4,7 +4,7 @@ public class GroundKill : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Barrel"))
+        if (other.CompareTag("barrel"))
         {
             GameManager.Instance.DestroyBarrel(other.gameObject);
         }
