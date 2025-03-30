8abbecb0c7da1b570bee211ac10459b9f7e22ba0
branden kennedy <brandenk88@gmail.com>
2025-03-30 15:37:01 -0700

    barrel spawner implemented on level 1, Barrel script edited so that barrels rolls along flat ground at a constant speed instead of relying on gravity.

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -3,22 +3,35 @@ using UnityEngine;
 public class Barrel : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField]private float speed = 6f;
+    [SerializeField] private float speed = 6f;
+    // This controls how much the barrel drops when it hits a border.
+    [SerializeField] private float dropForce = 3f; 
+    // 1 means moving right, -1 means moving left
+    private int horizontalDirection = 1;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void FixedUpdate()
+    {
+        // Maintain constant horizontal speed while preserving vertical velocity (gravity)
+        rb.linearVelocity = new Vector3(speed * horizontalDirection, rb.linearVelocity.y, 0);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("borderR"))
         {
-            rb.linearVelocity = new Vector3(-speed, rb.linearVelocity.y, 0); // Reverse X direction
+            horizontalDirection = -1;
+            // Apply a downward impulse so the barrel drops a bit
+            rb.AddForce(new Vector3(0, -dropForce, 0), ForceMode.VelocityChange);
         }
         else if (collision.gameObject.CompareTag("borderL"))
         {
-            rb.linearVelocity = new Vector3(speed, rb.linearVelocity.y, 0); // Reverse X direction
+            horizontalDirection = 1;
+            rb.AddForce(new Vector3(0, -dropForce, 0), ForceMode.VelocityChange);
         }
     }
 
