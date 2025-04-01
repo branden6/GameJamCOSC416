b18504fe1112520cc2da1790438387534a6bbd8c
branden kennedy <brandenk88@gmail.com>
2025-03-31 18:54:05 -0700

    updated end game and game over to show the banana, updated text for won game, added a skip cutscene

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -15,7 +15,6 @@ public class Goal : MonoBehaviour
             if (hud != null)
             {
                 hud.AddScore(1000);
-                // No need to set level here — GameManager will do it on scene load
             }
 
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
